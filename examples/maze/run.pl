e9e0af87339ee2e4b2c78b6d5008639d61e47727
Stefan Schiffer <schiffer@kbsg.rwth-aachen.de>
2007-10-14 13:04:30 +0000

    ** fixed lots of stupid bugs. ** put exo-stuff into single file (which is included in agent) ** generalized makefile, toplevel-ecl-Makefile still to come ** added ignore-props ** added xTra-predicates for go_*-actions to produce visual output ** cleaning up here and there
    stf
    
    
    git-svn-id: https://svn.kbsg.rwth-aachen.de/repos/kbsgolog@24 d9e49986-696a-4e79-9435-a9269e428f0d

--- examples/maze/run.pl
+++ examples/maze/run.pl
@@ -1,4 +1,79 @@
+/* ***************************************************************************
+ *  ,=³ .%%% %%%%%%%. .%%%%%.  .%%%%%.   %%%%%.%%. %. %%%%%%%% %% Rheinisch-
+ * [==].%%%   %%   %% %%   %%  %%   %%   %%  %%'%%.%%.%% %% %% %% Westfälische
+ *  //l%%%    %%   %% %%%%. ' %%       @ %%%%%' '%%%%%%% %% %%%%% Technische
+ * [==]%%     %%|=%%   %%=>%  %%         %%  %%  '%%'%%% %% %% %% Hochschule
+ * [==]%%%    %%   %% . '%%%% %%  '%%%   %%   %%  '% '%% %% %% %% Aachen
+ * [==]'%%%   %%   %% %%   %%  %%   %%   http://kbsg.rwth-aachen.de/
+ * o^^o '%%% %%%%%%%' '%%%%%'O '%%%%%'   Knowledge Based Systems Group
+ * ***************************************************************************
+ *
+ *           $Id:$
+ *        author: Stefan Schiffer <schiffer@cs.rwth-aachen.de>
+ *   description: run-time file.
+ *
+ * ************************************************************************ */
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% load everything we need:
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% connection to execution system
 :- ensure_loaded("xtra.pl").
+%% the agent
 :- ensure_loaded("agent.readylog").
+%% the pre-processed agent
 :- ensure_loaded("processed_agent.pl").
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% wanna autorun?
+%:- go.
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  TEST shortuts
+
+%:- spy transPr.
+%:- trace.
+%:- printf("Here!\n", []), gos.
+
+t:- icp([if(f_is_pos(0,0), cout("1"), cout("2"))]).
+
+t0 :- icp([exogf_Update, g, cout(pos)]).
+
+t1 :- icp([?(pos=[X, Y]), cout(X), cout(Y)]).
+
+t2 :- icp([if(and([ pos = [X,Y],
+                  f_is_pos(X,Y),
+                  Y2 is Y + 1,
+                  f_is_pos(X,Y2),
+                  f_is_wall(X,Y,X,Y2)
+                     ]), cout("1"), cout("2"))]).
+
+t3 :- 
+	icp([ if(and([ pos = [X,Y],
+	               is_pos(X,Y),
+	               Y2 is Y + 1,
+	               is_pos(X,Y2),
+	               not is_wall(X,Y,X,Y2)
+		   ]), 
+		   cout("1"), cout("2"))
+	       ]).
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Shortcuts (for convenience)
+
+vis :-
+	display,
+	draw_start(1,1),
+	draw_goal(5,5).
+
+go_prim  :- icp( solve( goto(5,5), 10, reward ) ).
+go_det   :- icp( solve( goto_det(5,5), 10, reward(5,5) ) ).
+go_stoch :- icp( solve( goto_stoch(5,5), 10, reward(5,5) ) ).
+go_stocheu :- icp( solve( goto_stoch(5,5), 9, reward_heuristic(5,5) ) ).
+go_memo  :- icp( solve( goto_memo(3,3), 7, reward(3,3) ) ).
+go_vis   :- icp( solve( goto_vis(3,3), 7, reward(3,3) ) ).
+go_visit :- icp( solve( goto_vis(3,3), 7, reward_heuristic(3,3) ) ).
 
--- examples/maze/xtra.pl
+++ examples/maze/xtra.pl
@@ -1,4 +1,18 @@
 /* ***************************************************************************
+ *  ,=³ .%%% %%%%%%%. .%%%%%.  .%%%%%.   %%%%%.%%. %. %%%%%%%% %% Rheinisch-
+ * [==].%%%   %%   %% %%   %%  %%   %%   %%  %%'%%.%%.%% %% %% %% Westfälische
+ *  //l%%%    %%   %% %%%%. ' %%       @ %%%%%' '%%%%%%% %% %%%%% Technische
+ * [==]%%     %%|=%%   %%=>%  %%         %%  %%  '%%'%%% %% %% %% Hochschule
+ * [==]%%%    %%   %% . '%%%% %%  '%%%   %%   %%  '% '%% %% %% %% Aachen
+ * [==]'%%%   %%   %% %%   %%  %%   %%   http://kbsg.rwth-aachen.de/
+ * o^^o '%%% %%%%%%%' '%%%%%'O '%%%%%'   Knowledge Based Systems Group
+ * ***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ *
+ * ***************************************************************************
  *
  *           $Id:$
  *        author: Stefan Schiffer <schiffer@cs.rwth-aachen.de>
@@ -7,9 +21,18 @@
  *
  * ************************************************************************ */
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+/** exogf_Update.
+ */
+xTra(exogf_Update, _H) :- !, 
+	printf(" *** exogf_Update *** \n", []), flush(output).
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  cout in various variations.
+
 xTra(cout(V), _H) :- !,
 	printf("%w", [V]), flush(output).
-
 
 xTra(cout(F, V), _H) :- !,
 	printf(F, V), flush(output).
@@ -17,5 +40,46 @@ xTra(cout(F, V), _H) :- !,
 xTra(cout(Color,F,V),_H) :- !,
 	printColor(Color,F,V).
 
-xTra(Action,_S) :-
-	printColor( red, " xTra: DON'T KNOW HOW TO HANDLE '%w'\n", [Action]).
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  primitive actions in maze.
+%%  includes calls to draw-routines
+
+xTra(go_right,H) :- 
+	printf(" EXEC: go_right", []), 
+	has_val( pos, V, H ), 
+	V = [X,Y], 
+	printf(" at %w \n", [V]), 
+	draw_action("R", X, Y).
+xTra(go_left,H)  :- 
+	printf(" EXEC: go_left", []), 
+	has_val( pos, V, H ), 
+	V = [X,Y], 
+	printf(" at %w \n", [V]), 
+	draw_action("L", X, Y).
+xTra(go_up,H)    :- 
+	printf(" EXEC: go_up", []), 
+	has_val( pos, V, H ), 
+	V = [X,Y], 
+	printf(" at %w \n", [V]), 
+	draw_action("U", X, Y).
+xTra(go_down,H)  :- 
+	printf(" EXEC: go_down", []), 
+	has_val( pos, V, H ), 
+	V = [X,Y], 
+	printf(" at %w \n", [V]), 
+	draw_action("D", X, Y).
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+update :- 
+	printf(" --- UPDATE ...\n", []), 
+	refresh_window,
+	printf(" --- UPDATE ... done.\n", []).
+
+
+
+%% ABSORBER ENTRY %%%%%%%%%%%%%%%%%%%%%%%%
+%% used to catch all the stuff
+%% that doesn't have an own handle
+xTra(Action,Situation) :-
+	printColor( red, " xTra: DON'T KNOW HOW TO HANDLE '%w' IN SIT: '%w'\n", [Action,Situation]).
