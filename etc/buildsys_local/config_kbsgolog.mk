deaaccb3d1e1e7dfc1b7db6e704995eb51f27e78
Stefan Schiffer <schiffer@kbsg.rwth-aachen.de>
2010-06-22 11:39:10 +0000

    ** unified eclipse related stuff by using $(eclipsebin) **  instead of some hardcoded left-overs
    stf
    
    
    git-svn-id: https://svn.kbsg.rwth-aachen.de/repos/kbsgolog@292 d9e49986-696a-4e79-9435-a9269e428f0d

--- etc/buildsys_local/config_kbsgolog.mk
+++ etc/buildsys_local/config_kbsgolog.mk
@@ -10,13 +10,15 @@
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; version 2 of the License.
+#   the Free Software Foundation; either version 2 of the License or
+#   (at your option) any later version.
 #
 #*****************************************************************************
 
+ECLIPSEBIN = eclipse-clp
+
 #ifeq ($(shell which eclipse 2>&1 | grep -v "which: no"),)
 #ifneq ($(shell ls $(ECLIPSE_LOC)/eclipse.ini 2>&1 | grep -v "ls:"),)
-
 
 #ifeq ($(shell which eclipse; if [ "$$?" = "0" ]; then echo "yes"; else echo "no"; fi),no)
 #  #echo -e " **** have eclipse!" 
@@ -26,19 +28,20 @@
 #  #$(shell echo " got NO eclipse" )
 #endif
 
-HAVE_ECLIPSE = $(if $(shell which eclipse; echo $${?/1/}),1,0)
+#HAVE_ECLIPSE = $(if $(shell which eclipse; echo $${?/1/}),1,0)
+HAVE_ECLIPSE = $(if $(shell which $(ECLIPSEBIN); echo $${?/1/}),1,0)
 
 ifeq ($(HAVE_ECLIPSE),1)
    ## we got eclipse, but which one is it? 
    #$(warning blub)
-   ECLIPSE_LOC = $(shell which eclipse)
+   ECLIPSE_LOC = $(shell which $(ECLIPSEBIN))
    ECLIPSE_CLP = $(if $(shell ls $(ECLIPSE_LOC)/eclipse.ini > /dev/null 2>&1; echo $${?/1/}),1,0)
    ifeq ($(ECLIPSE_CLP),1)
      ## got the right eclipse
      ## override stuff from buildsys/config.mk
-     ECLIPSE_ARCH    = $(shell eclipse -e "get_flag('hostarch',V),write(V)")
-     ECLIPSE_PATH    = $(shell eclipse -e "get_flag('installation_directory',V),write(V)")
-     ECLIPSE_VERSION = $(shell eclipse -e "get_flag('version',V),write(V)")
+     ECLIPSE_ARCH    = $(shell $(ECLIPSEBIN) -e "get_flag('hostarch',V),write(V)")
+     ECLIPSE_PATH    = $(shell $(ECLIPSEBIN) -e "get_flag('installation_directory',V),write(V)")
+     ECLIPSE_VERSION = $(shell $(ECLIPSEBIN) -e "get_flag('version',V),write(V)")
    else
      ## got the wrong eclipse
      ECLIPSE_ARCH    = 
