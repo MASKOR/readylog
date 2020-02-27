ecdc33e3e077d89d3e853f92b825aba245332d41
Victor Mataré <matare@fh-aachen.de>
2020-02-27 22:16:39 +0100

    remove non-GPL libs and dependent bits

--- examples/ipltest/ipl_processed_agent.pl
+++
@@ -1,7 +0,0 @@
-/*******************************
-* file generated from ReadyLog *
-*******************************/
-
-:- pragma(nodebug).
-prolog_ipl_proc( testproc ).
-prolog_ipl_proc( testproc ) :-[[testtoken, anothertesttoken, if(true, [solve([do1, deterministicAction, nondet([doA, doB])], 3, rewardf)], []), dwajdo, blablubb]].
